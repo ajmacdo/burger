a4cd9aaaad007c4930c2ffbc486d4bfa0434610a
aj mac <macdonalda47@gmail.com>
2020-08-11 14:38:54 -0500

    create, organize files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # burger
+
+page w/ input field and submit button
+
+user types some kind of burger into the input field
+user hits submit button
+
+what was typed is saved in mysql db
+it is also displayed on screen next to input field
+there is a "devour it" button next to displayed content
+
+when button is clicked, the displayed content moves to a different column/area on the page.
+
--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,17 @@
 -- Drops the burger if it exists currently --
-DROP DATABASE IF EXISTS burger;
+DROP DATABASE IF EXISTS burgers_db;
 -- Creates the "burger" database --
-CREATE DATABASE burger;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  flavor VARCHAR(45) NULL,
+  price DECIMAL(10,2) NULL,
+  quantity INT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (flavor, price, quantity)
+VALUES ("vanilla", 2.50, 100);
